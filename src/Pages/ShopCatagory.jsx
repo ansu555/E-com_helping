64bbfee722ba9550b715a1db412f1a3835f98e2a
ansu555 <anik200365@gmail.com>
2024-11-23 03:51:00 +0530

    css

--- src/Pages/ShopCatagory.jsx
+++ src/Pages/ShopCatagory.jsx
@@ -1,41 +1,45 @@
-import React, { useContext } from 'react';
-import './Css/ShopCatagory.css';
-import { Shopcontext } from '../Context/Shopcontext';
-import dropdowm_icon from '../Components/Items/dropdown_icon.png';
-import Item from '../Components/Item/Item';
+import React, { useContext } from "react";
+import "./Css/ShopCatagory.css";
+import { Shopcontext } from "../Context/Shopcontext";
+import dropdown_icon from "../Components/Items/dropdown_icon.png";
+import Item from "../Components/Item/Item";
 
 function ShopCatagory(props) {
-  // Get products from context
   const { all_product } = useContext(Shopcontext) || { all_product: [] }; // Default to empty array if context is null
-  
-  // Log the category to debug if it's being passed correctly
-  console.log("Category prop:", props.category);
 
   // Filter products by category
-  const filteredProducts = all_product.filter(item => item.category === props.category);
-  
-  // Log filtered products to debug
-  console.log("Filtered Products:", filteredProducts);
-  
+  const filteredProducts = all_product.filter(
+    (item) => item.category.toLowerCase() === props.category.toLowerCase()
+  );
+
   return (
-    <div className='shop-catagory'>
-      <img src={props.banner} alt="Shop category banner" />
-      
-      <div className='shopcategory-indexsort'>
+    <div className="shop-catagory">
+      {/* Banner Section */}
+      <div className="shop-banner-container">
+        <img
+          className={`banner ${props.category}-banner`}
+          src={props.banner}
+          alt={`${props.category} category banner`}
+        />
+      </div>
+
+      {/* Header Section */}
+      <div className="shopcategory-indexsort">
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span>Showing 1-{filteredProducts.length}</span> out of{" "}
+          {filteredProducts.length} products
         </p>
-        
         <div className="shopcategory-sort">
-          Sort by <img src={dropdowm_icon} alt="Sort options" />
+          Sort by <img src={dropdown_icon} alt="Sort options" />
         </div>
       </div>
 
+      {/* Product Section */}
       <div className="shopcategory-product">
         {filteredProducts.length > 0 ? (
-          filteredProducts.map(item => (
+          filteredProducts.map((item) => (
             <Item
-              key={item.id} // Use the unique `id` for key
+              key={item.id} // Unique key for each product
               id={item.id}
               name={item.name}
               image={item.image}
@@ -44,7 +48,7 @@ function ShopCatagory(props) {
             />
           ))
         ) : (
-          <p>No products available in this category.</p> // Show a message if no products are found
+          <p className="no-products">No products available for this category.</p>
         )}
       </div>
     </div>
